26ebaf21380da0eb442468bd380d28523b89109e
youya66 <youya66.official@gmail.com>
2019-01-12 21:46:15 +0900

    add model

--- app/routes/rentals.js
+++ app/routes/rentals.js
@@ -1,4 +1,20 @@
-import Route from '@ember/routing/route'
+import Route from "@ember/routing/route";
 
 export default Route.extend({
+  model() {
+    return [
+      {
+        name: "hoge",
+        city: "TOKYO"
+      },
+      {
+        name: "fuga",
+        city: "USA"
+      },
+      {
+        name: "piyo",
+        city: "CHINA"
+      }
+    ];
+  }
 });
